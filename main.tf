fa5b4a09068f9e5a4c473fde9be378ba10e8ede4
CC <alfanocloud@gmail.com>
2023-09-07 19:28:15 +0000

    add local tf files to test branch

--- main.tf
+++ main.tf
@@ -7,15 +7,28 @@ provider "aws" {
   }
 }
 
+#outputs
+output "subnet_id_set" {
+  value = data.aws_subnets.private.ids
+}
+
+#data sources
+data "aws_subnets" "private" {
+  filter {
+    name = "vpc-id"   
+    values = [var.my-vpc-id]
+  }
+  tags = {
+    Data = "subnet"
+  }
+  depends_on = [aws_subnet.private-subnet]
+}
+
 #local variables
 locals {
   cidrs-azs = {
     private-subnet-1 = {cidr = var.ps1c, zone = "us-east-1a"}
     private-subnet-2 = {cidr = var.ps2c, zone = "us-east-1b"}
-  }
-  instances = {
-    app-host-1 = ""
-    app-host-2 = ""
   }
 }
 
@@ -27,20 +40,20 @@ resource "aws_subnet" "private-subnet" {
   availability_zone = each.value.zone
   tags = {
     Name = each.key
+    Data = "subnet"
   }
 }
 
 # app host instances
 resource "aws_instance" "app-host" {
-  for_each = local.instances
   count = 2
   ami = var.ah-ami
   vpc_security_group_ids = [aws_security_group.allow-elb-bastion-apphost.id]
   instance_type = "t2.micro"
-  subnet_id = aws_subnet.private-subnet[count.index].id
-  #depends_on = [aws_subnet.private-subnet]
+  key_name = "bastion-access"
+  subnet_id = data.aws_subnets.private.ids[count.index]
   tags = {
-    Name = each.key
+    Name = "app-host-${count.index}"
   }
 }
 
@@ -53,14 +66,14 @@ resource "aws_security_group" "allow-elb-bastion-apphost" {
     from_port = 443
     to_port = 443
     protocol = "tcp"
-    security_groups = [aws_security_group.allow-web-elb.id]
+    security_groups = []
   }
   ingress {
     description = "HTTP"
     from_port = 80
     to_port = 80
     protocol = "tcp"
-    security_groups = [aws_security_group.allow-web-elb.id]
+    security_groups = []
   }
   ingress {
     description = "SSH"
@@ -68,88 +81,6 @@ resource "aws_security_group" "allow-elb-bastion-apphost" {
     to_port = 22
     protocol = "tcp"
     security_groups = [var.bastion-host-sg]
-  }
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-  }
-}
-
-#nat gateway elastic ip
-resource "aws_eip" "nat-eip" {
-  vpc = true
-  depends_on = var.igw-id
-}
-
-#nat gateway
-resource "aws_nat_gateway" "NAT-gateway" {
-  allocation_id = aws_eip.nat-eip.id
-  subnet_id = var.public-subnet-id
-  connectivity_type = "public"
-}
-
-#nat gateway route to internet
-resource "aws_route" "nat-out" {
-  route_table_id = var.my-route-table-id
-  destination_cidr_block = 0.0.0.0/0
-  nat_gateway_id = aws_nat_gateway.NAT-gateway.id
-}
-
-#load balancer
-resource "aws_lb" "public-elb" {
-  internal = false
-  load_balancer_type = "network"
-  subnets = var.public-subnet-id
-  security_group = aws_security_group.allow-web-elb.id
-  enable_deletion_protection = true
-  enable_cross_zone_load_balancing = true
-  tags = {
-    Name = "public-elb"
-  }
-}
-
-#load balancer target group
-resource "aws_lb_target_group" "lb-tg" {
-  name = "lb-target-group"
-  port = "80"
-  vpc_id = var.my-vpc-id
-  health_check {
-    healthy_threshold = "3"
-    interval = "30"
-    protocol = "HTTP"
-  }
-  depend_on = [aws_lb.public-elb]
-}
-
-#target group association
-resource "aws_target_group_attachment" "elb-targets" {
-  for_each {
-    for k, v in aws_instance.app-host :
-    v.id => v
-  }
-  target-group-arn = aws_lb_target_group.lb-tg.arn
-  target_id = each.value.id
-  port = 80
-}
-
-#load balancer security group
-resource "aws_security_group" "allow-web-elb" {
-  description = "Allow inbound traffic on ports 80, 443"
-  vpc-id = var.my-vpc-id
-  ingress {
-    description = "HTTPS"
-    form_port = 443
-    to_port = 443
-    protocol = "tcp"
-    cidr_blocks = [0.0.0.0/0]
-  }
-  ingress {
-    description = "HTTP"
-    form_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = [0.0.0.0/0]
   }
   egress {
     from_port = 0
--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "my-vpc-id" {
   description = "VPC ID"
-  default = "vpc-07989cc04f4966ee5"
+  default     = "vpc-07989cc04f4966ee5"
 }
 
 variable "ps1c" {
   description = "private subnet 1 cidr"
-  default = "172.31.1.0/24"
+  default     = "172.31.1.0/24"
 }
 
 variable "ps2c" {
@@ -21,19 +21,4 @@ variable "ah-ami" {
 variable "bastion-host-sg" {
   description = "bastion host security group"
   default = "sg-038074cd79522a0d2"
-}
-
-variable "public-subnet-id" {
-  description = "public subnet id"
-  default = "subnet-03c14ae5b0481b727"
-}
-
-variable "my-route-table-id" {
-  description = "default route table id"
-  default = "rtb-0979817af510a87e4"
-}
-
-variable "igw-id" {
-  description = "internet gateway ID"
-  default = "igw-07d04803c9c40f77f"
 }
